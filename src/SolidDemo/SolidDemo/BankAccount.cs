c93a3f9b20aa93fd3a97442ea69500dd941ff5e6
Cyrus Penuliar <Cyrus.Penuliar@infor.com>
2024-07-02 08:44:38 +0800

    Initial

---
+++ src/SolidDemo/SolidDemo/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿public class BankAccount(int accountId, string accountHolderName, decimal balance)
+{
+    private decimal _balance = balance;
+
+    public int AccountId { get; } = accountId;
+    public string AccountHolderName { get; } = accountHolderName;
+
+    public decimal GetBalance()
+    {
+        return _balance;
+    }
+
+    public bool Withdraw(decimal amount)
+    {
+        if (amount <= 0 || amount > _balance)
+        {
+            Console.WriteLine("You have insufficient amount");
+
+            return false;
+        }
+
+        _balance -= amount;
+        Console.WriteLine($"Withdrawal of {amount:f2} from Account ID {AccountId}");
+        return true;
+    }
+}
---
+++ src/SolidDemo/SolidDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SolidDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var bankAccount = new BankAccount(1, "Juan Dela Cruz", 100m);
+
+        bankAccount.Withdraw(50m);
+        var balance = bankAccount.GetBalance();
+
+        Console.WriteLine($"Your remaining Balance is {balance:f2}");
+
+        bankAccount.Withdraw(100m);
+
+        Console.ReadLine();
+    }
+}
